cffa0b5d91f21e4c2cf43840b910cf143b0a7c27
pedro lima <pedro.lima.wk@gmail.com>
2022-04-01 16:26:31 -0300

    initial commit

---
+++ File.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mickey
+{
+    public class File
+    {
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public DateTime? Date { get; set; }
+        public int? FileSize { get; set; }
+        public string? Word { get; set; }
+        public string? Phrase { get; set; }
+
+        public override string ToString()
+        {
+            return 
+                $"Name: {this.Name ?? ""}\n" +
+                $"Type: {this.Type ?? ""}\n" +
+                $"Date: {this.Date?.ToString("G") ?? ""}\n" +
+                $"File Size:{this.FileSize ?? null}\n" +
+                $"Word: {this.Word??""}\n" +
+                $"Phrase: {this.Phrase??""}\n";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mickey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new File()
+            {
+                Type = "txt",
+                Name = "algumarquivo",
+                Date = DateTime.Now,
+                FileSize = 123,
+                Word = "coisa",
+                Phrase = "alguma coisa",
+            };
+
+            Console.WriteLine(file.ToString());
+            Console.ReadLine();
+        }
+    }
+}
